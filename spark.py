270ca7ee2fd4f6703dd4f487a6b73fd648855008
Gabriel Sab <gabrielsab@poli.ufrj.br>
2018-08-25 14:22:50 -0300

    Added system platform check to spark.py so that we don't have to change pyspark python path. Also added scripts for running zookeeper and kafka and creating our kafka topic.

--- spark.py
+++ spark.py
@@ -8,7 +8,14 @@ from pyspark.sql import SparkSession
 import pprint
 import json
 from pyspark.sql.functions import col, from_json
-os.environ["PYSPARK_PYTHON"]      = "/usr/local/bin/python3"
+
+import platform
+system_name = platform.system()
+if system_name == 'Linux':
+    os.environ["PYSPARK_PYTHON"] = "/usr/bin/python3"
+else:
+    os.environ["PYSPARK_PYTHON"] = "/usr/local/bin/python3"
+
 # os.environ['PYSPARK_SUBMIT_ARGS'] = '--jars spark-sql-kafka-0-10_2.11-2.1.0.jar spark-streaming-kafka-0-8-assembly_2.11-2.3.1.jar pyspark-shell'
 
 # sc  = SparkContext(appName="PythonStreamingRecieverKafka")
