d55af5858aee1bea5ef417d91300bfd4d8fc1e16
Pamela Tabak <pamelatabak@poli.ufrj.br>
2018-08-26 17:32:46 -0300

    getting sentiment value for each entity

--- spark.py
+++ spark.py
@@ -3,7 +3,7 @@
 #from pyspark.streaming.kafka import KafkaUtils
 from pyspark.sql.types import *
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, from_json, explode, split, udf, window
+from pyspark.sql.functions import col, from_json, explode, split, udf, window, split, collect_list
 
 import sys
 import os
@@ -20,16 +20,13 @@ else:
 from textblob import TextBlob
 import spacy
 
-nlps = {}
-nlps['en'] = spacy.load('en')
-nlps['pt'] = spacy.load('pt')
-nlps['es'] = spacy.load('es')
+nlps 	           = {}
+nlps['en']         = spacy.load('en')
+nlps['pt']         = spacy.load('pt')
+nlps['es'] 		   = spacy.load('es')
+checkpointLocation = "/Users/pamelatabak/Documents/PESC_UFRJ/Banco_de_Dados/news-visualization-with-spark/checkpoint"
+path 			   = "/Users/pamelatabak/Documents/PESC_UFRJ/Banco_de_Dados/news-visualization-with-spark/output"
 
-# os.environ['PYSPARK_SUBMIT_ARGS'] = '--jars spark-sql-kafka-0-10_2.11-2.1.0.jar spark-streaming-kafka-0-8-assembly_2.11-2.3.1.jar pyspark-shell'
-
-# sc  = SparkContext(appName="PythonStreamingRecieverKafka")
-# sc.setLogLevel("WARN")
-# ssc = StreamingContext(sc, 2) # 2 second window
 json_schema = StructType([
     StructField("body", StringType()),
     StructField("title", StringType()),
@@ -48,7 +45,7 @@ inData = spark.readStream.format("kafka")\
     .option("maxOffsetsPerTrigger", 100)\
     .option("subscribe","news").load()
 data   = inData.select(from_json(col("value").cast("string"), json_schema).alias("parsed_value"))
-data   = data.select("parsed_value.*")
+data   = data.select("parsed_value.*")#.filter((col("region") != "canada")) #news might be in france, we are not interested 
 
 def entity_is_good(entity, lang):
     # check if empty
@@ -66,36 +63,64 @@ def entity_is_good(entity, lang):
     return True
 
 def extract_entities(body, region):
+    entity_positions  = {}
+    sentence_ends 	  = []
+    entity_sentiments = {}
     if region == 'brazil':
         lang = 'pt'
-    elif region == 'united states' or region == 'canada':
+    elif region == 'united states':
         lang = 'en'
     else:
         lang = 'es'
+    
+    blob = TextBlob(body)
+    for sentence in blob.sentences:
+        sentence_ends.append(sentence.end_index)
+    sentence_ends.sort()
+
     nlp_model = nlps[lang]
-    doc = nlp_model(body.replace('\r', ' ').replace('\n', ' '))
-    entities = [ent.text.strip() for ent in doc.ents if entity_is_good(ent.text, lang)]
-    return entities
-    #return [token.strip() for token in body.replace('\n', ' ').replace('\r', ' ').split() if token.strip()]
+    doc 	  = nlp_model(body.replace('\r', ' ').replace('\n', ' '))
+    entities  = [ent.text.strip() for ent in doc.ents if entity_is_good(ent.text, lang)]
+    for ent in doc.ents:
+        cleaned_ent = ent.text.strip()
+        if entity_is_good(cleaned_ent, lang):
+            if cleaned_ent not in entity_positions:
+                entity_positions[cleaned_ent] = []
+            entity_positions[cleaned_ent].append(ent.start_char)
+    for entity in entity_positions:
+        if entity not in entity_sentiments:
+            entity_sentiments[entity] = []
+        for position in entity_positions[entity]:
+            sentence_idx = 0
+            while position > sentence_ends[sentence_idx]:
+                sentence_idx += 1
+                if sentence_idx > len(sentence_ends) - 1:
+                    break
+            if sentence_idx <= len(sentence_ends) - 1:
+                sentence = blob.sentences[sentence_idx]
+                entity_sentiments[entity].append(sentence.sentiment.polarity)
+    ret = []
+    for ent in entity_sentiments:
+        sentiments = entity_sentiments[ent]
+        for s in sentiments:
+            ret.append((str(ent) + "---" + str(s)))
+    return ret
 
 extract_entities_udf = udf(extract_entities, ArrayType(StringType()))
 
-data = data.withColumn('entity', explode(extract_entities_udf('body', 'region')))\
-    .withWatermark("timestamp", "1 minute")\
+data = data.withColumn('entitysentiment', explode(extract_entities_udf('body', 'region')))
+
+split_col = split(data['entitysentiment'], '---')
+data 	  = data.withColumn('entity', split_col.getItem(0))
+data      = data.withColumn('sentiment', split_col.getItem(1))
+data      = data.select("entity", "sentiment", "region", "timestamp").withWatermark("timestamp", "1 minute")\
     .groupBy(window("timestamp", "2 minutes", "2 minutes"), 'region', 'entity')\
-    .count()#\
-    #.sort('count', ascending=False)
+    .agg(collect_list("sentiment"))
 
-#data   = data.withColumn('word', explode(split(col('body'), ' ')))\
-#    .groupBy('word')\
-#    .count()\
-#    .sort('count', ascending=False)
-#query.printSchema()
-
-#data.writeStream.outputMode("complete").format("console").start().awaitTermination()
-data.writeStream.outputMode("append")\
-    .format("json")\
-    .option("checkpointLocation", "/home/gabriel/dev/bigdata/news-visualization-with-spark/checkpoint") \
-    .option("path", "/home/gabriel/dev/bigdata/news-visualization-with-spark/output")\
-    .start()\
-    .awaitTermination()
+data.writeStream.outputMode("complete").format("console").start().awaitTermination()
+# data.writeStream.outputMode("append")\
+#     .format("json")\
+#     .option("checkpointLocation", checkpointLocation) \
+#     .option("path", path)\
+#     .start()\
+#     .awaitTermination()
